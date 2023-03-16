fa10012fb392a3b09d2ff743ef961a98a00e8c0e
Sravan T P <sravantp03@gmail.com>
2023-03-16 15:55:40 +0530

    initial project setup

---
+++ my-app/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "LW3Punks NFT Collection",
+  description: "NFT Collections",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ my-app/app/page.js
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div className={styles.main}>
+        <h1>LW3Punks</h1>
+      </div>
+    </main>
+  );
+}
